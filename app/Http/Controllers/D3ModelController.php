e20720dac8862cd98d347d1ed2aa3b2ed723e17a
rq0net <70127497+rq0net@users.noreply.github.com>
2022-10-10 15:01:29 -0700

    add administrator

---
+++ app/Http/Controllers/D3ModelController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreD3ModelRequest;
+use App\Http\Requests\UpdateD3ModelRequest;
+use App\Models\D3Model;
+
+class D3ModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreD3ModelRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreD3ModelRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\D3Model  $d3Model
+     * @return \Illuminate\Http\Response
+     */
+    public function show(D3Model $d3Model)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\D3Model  $d3Model
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(D3Model $d3Model)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateD3ModelRequest  $request
+     * @param  \App\Models\D3Model  $d3Model
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateD3ModelRequest $request, D3Model $d3Model)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\D3Model  $d3Model
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(D3Model $d3Model)
+    {
+        //
+    }
+}
